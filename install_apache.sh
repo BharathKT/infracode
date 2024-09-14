90f97cd705fa4931e0057102f3dcad0c0a6c348d
BharathKT <90966025+BharathKT@users.noreply.github.com>
2024-09-14 18:05:54 +0800

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -4,7 +4,7 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-curl www.google.com > /tmp/index.html
+curl www.oselabs.com > /tmp/index.html
 cp /tmp/index.html /var/www/html/index.html
 sudo systemctl restart httpd
 #echo "Hello World from $(hostname -f)" > /var/www/html/index.html
